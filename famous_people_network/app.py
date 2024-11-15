ed344c10689e0c11155bf9f308348f397ecbe786
Michael Hu <mbh326684@gmail.com>
2024-11-15 15:03:44 -0500

    simple layout structure

--- famous_people_network/app.py
+++ famous_people_network/app.py
@@ -14,46 +14,53 @@ app = Dash(__name__)
 people_network = PeopleNetwork()
 
 app.layout = html.Div(
-    children=[
+    [
         html.Div(
-            [
+            style={"padding": 10, "display": "flex", "flexDirection": "row"},
+            children=[
                 html.Div(
-                    [
-                        "Person Name: ",
-                        dcc.Input(id="input-person", value="", type="text"),
-                    ]
+                    style={"flex": 1},
+                    children=[
+                        html.H1("Famous People Network"),
+                        html.Div(
+                            [
+                                html.P("Person Name: "),
+                                dcc.Input(id="input-person", value="", type="text"),
+                            ]
+                        ),
+                        html.Div(
+                            [
+                                html.P("Depth: "),
+                                dcc.Slider(id="slider-depth", min=0, max=5, step=1, value=0),
+                            ]
+                        ),
+                        html.Button(id="button-submit-state", disabled=False, children="Submit"),
+                        html.Div(id="graph-info"),
+                    ],
                 ),
-                html.Div(
-                    [
-                        "Depth: ",
-                        dcc.Slider(id="slider-depth", min=0, max=5, step=1, value=0),
-                    ]
+                cyto.Cytoscape(
+                    id="people-network",
+                    layout={"name": "preset", "directed": True},
+                    style={"height": "770px", "flex": 3, "border": "1px solid black"},
+                    stylesheet=[
+                        {
+                            "selector": "node",
+                            "style": {
+                                "label": "data(name)",
+                            },
+                        },
+                        {
+                            "selector": "edge",
+                            "style": {
+                                # The default curve style does not work with certain arrows
+                                "curve-style": "straight",
+                                "target-arrow-shape": "triangle",
+                            },
+                        },
+                    ],
                 ),
-                html.Button(id="button-submit-state", disabled=False, children="Submit"),
-            ]
-        ),
-        cyto.Cytoscape(
-            id="people-network",
-            layout={"name": "preset", "directed": True},
-            style={"width": "100%", "height": "600px"},
-            stylesheet=[
-                {
-                    "selector": "node",
-                    "style": {
-                        "label": "data(name)",
-                    },
-                },
-                {
-                    "selector": "edge",
-                    "style": {
-                        # The default curve style does not work with certain arrows
-                        "curve-style": "straight",
-                        "target-arrow-shape": "triangle",
-                    },
-                },
             ],
-        ),
-        html.Div(id="graph-info"),
+        )
     ]
 )
 
@@ -72,12 +79,20 @@ def add_person(n_clicks, person_name, depth):
     return people_network.to_ctyoscape()["elements"]
 
 
-@callback(Output("graph-info", "children"), Input("people-network", "tapNodeData"))
-def display_node_page(data):
-    if data is None:
-        raise PreventUpdate
-    page = people_network.get_page(data["id"])
-    return page.summary
+@callback(
+    Output("graph-info", "children"),
+    Input("people-network", "selectedNodeData"),
+    Input("people-network", "selectedEdgeData"),
+)
+def display_node_page(selected_nodes, selected_edges):
+    if selected_nodes and selected_edges:
+        return "node and edge"
+    elif selected_nodes:
+        node = selected_nodes[-1]
+        title = node["name"]
+        return people_network.get_page(title).summary
+    elif selected_edges:
+        return "edge"
 
 
 if __name__ == "__main__":
